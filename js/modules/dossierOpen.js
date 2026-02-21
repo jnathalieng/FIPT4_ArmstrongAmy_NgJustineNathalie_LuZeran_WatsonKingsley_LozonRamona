c6fb3802a48a78845cf893be75978eeaebf7b1ff
Ramona <144703303+Ramona-Lozon@users.noreply.github.com>
2026-02-21 15:31:31 -0500

    altered dossiers

--- js/modules/dossierOpen.js
+++ js/modules/dossierOpen.js
@@ -1,15 +1,17 @@
 export function dossierOpen() {
 
-    document.querySelector('.dossier-tag').addEventListener('mouseenter', function() {
-        console.log("mouse entering");
+    // document.querySelector('.dossier-tag').addEventListener('mouseenter', function() {
+    //     console.log("mouse entering");
 
-        document.querySelector('#cover').classList.add('hideCover');
-    });
+    //     document.querySelector('#cover').classList.add('hideCover');
+    // });
 
-    document.querySelector('.dossier-tag').addEventListener('mouseleave', function() {
+    // document.querySelector('.dossier-tag').addEventListener('mouseleave', function() {
 
-        console.log("mouse leaving");
-        document.querySelector('#cover').classList.remove('.hideCover');
-    });
+    //     console.log("mouse leaving");
+    //     document.querySelector('#cover').classList.remove('.hideCover');
+    // });
 
+
+    
 }
