6ece97a6d9b2711223b5c3b052e2a37acacdb701
Zeran Lu <zeranlu0326@gmail.com>
2026-02-20 23:12:54 -0500

    css animation of dossiers opening

---
+++ js/modules/dossierOpen.js
@@ -0,0 +1,15 @@
+export function dossierOpen() {
+
+    document.querySelector('.dossier-tag').addEventListener('mouseenter', function() {
+        console.log("mouse entering");
+
+        document.querySelector('#cover').classList.add('hideCover');
+    });
+
+    document.querySelector('.dossier-tag').addEventListener('mouseleave', function() {
+
+        console.log("mouse leaving");
+        document.querySelector('#cover').classList.remove('.hideCover');
+    });
+
+}
