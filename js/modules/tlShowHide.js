2821cd6a343c14aeab81e1ea18377e64678a53ee
Zeran Lu <zeranlu0326@gmail.com>
2026-03-02 19:31:54 -0500

    added scrollIntoView functionality to toggle button

--- js/modules/tlShowHide.js
+++ js/modules/tlShowHide.js
@@ -1,11 +1,19 @@
 export function tlShowHide() {
-
     const tlToggleButton = document.querySelector('.tl-toggle-btn');
     const gammaCons = document.querySelectorAll('.tl-gamma-con');
 
     tlToggleButton.addEventListener('click', function() {
-        gammaCons.forEach(gammaCon =>
-            gammaCon.classList.toggle('tlShowHide'));
-            console.log("button clicked");
-        })
+        gammaCons.forEach(gammaCon => {
+            gammaCon.classList.toggle('tlShowHide');
+            
+            if (gammaCon.classList.contains('tlShowHide')) {
+                gammaCon.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        });
+        
+        console.log("button clicked");
+    })
 }
