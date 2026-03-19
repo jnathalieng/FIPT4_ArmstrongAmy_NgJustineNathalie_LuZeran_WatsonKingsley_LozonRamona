53b99e5bfed560603e60efec5cd4e2319c0d7bec
Zeran Lu <zeranlu0326@gmail.com>
2026-03-19 11:22:07 -0400

    JS: added height and velocity calculating variables and function to create parallax effects

--- js/modules/tlParallaxObj.js
+++ js/modules/tlParallaxObj.js
@@ -1,5 +1,31 @@
 export function parallaxObj() {
     gsap.registerPlugin(ScrollTrigger);
 
-    
+    // function to get y translation values based on container height
+    const getY = (element) => {
+        const height = element.clientHeight;
+        const maxScrollSpeed = -300;
+        const minScrollSpeed = -100;
+        const referenceHeight = 500;
+
+        // function to adjust velocities based on heights relative to reference height
+        const speedFactor = maxScrollSpeed + (height / referenceHeight) * (minScrollSpeed - maxScrollSpeed);
+
+        console.log({height, speedFactor});
+        return speedFactor;
+    };
+
+    document.querySelectorAll(".parallax-con-text").forEach((textBox) => {
+        gsap.to(textBox, {
+            y: getY(textBox),
+            ease: "none",
+            scrollTrigger: {
+                trigger: textBox,
+                start: "top bottom",
+                end: "bottom top",
+                scrub: 1
+            }
+        });
+    });
+
 }
