7ff60c48f359fe738680f9233c064f95f58322f3
Zeran Lu <zeranlu0326@gmail.com>
2026-03-19 14:18:43 -0400

    TL: added JS variables

--- js/modules/tlImgTextReveal.js
+++ js/modules/tlImgTextReveal.js
@@ -1,3 +1,8 @@
 export function tlImgTextReveal() {
-    console.log("going to reveal text");
-}
+    
+    const iboxText = document.querySelectorAll(".tl-ibox-p");
+    const textHeight = iboxText.scrollHeight;
+
+    const hoverImg = document.querySelectorAll(".tl-ibox-pic-con");
+
+};
