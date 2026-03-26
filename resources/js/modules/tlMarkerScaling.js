09c27dcc863afc89c3792196fd2550fee2eff611
Zeran Lu <zeranlu0326@gmail.com>
2026-03-26 19:46:57 -0400

    Migration of positional data

--- resources/js/modules/tlMarkerScaling.js
+++ resources/js/modules/tlMarkerScaling.js
@@ -1,7 +1,7 @@
 export function tlMarkerScaling() {
-    const markersPositionPercentage = [
-        0.23, 0.37, 0.51, 0.65, 0.79, 0.93
-    ];
+    // const markersPositionPercentage = [
+    //     0.23, 0.37, 0.51, 0.65, 0.79, 0.93
+    // ];
 
     const markers = document.querySelectorAll(".tl-marker");
     const maskingBar = document.querySelector("#tl-scroll-prog-mask");
