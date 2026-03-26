526ed972bbf8138bfda78d6e5e7e371bdf80333c
Zeran Lu <zeranlu0326@gmail.com>
2026-03-26 18:18:37 -0400

    JS: added forEach loop to scale the scaleY value to the defined coordinates

--- resources/js/modules/tlMarkerScaling.js
+++ resources/js/modules/tlMarkerScaling.js
@@ -1,5 +1,16 @@
 export function tlMarkerScaling() {
     const markersPositionPercentage = [
         0.2, 0.34, 0.48, 0.62, 0.76, 0.9
-    ]
+    ];
+
+    const markers = document.querySelectorAll(".tl-marker");
+    const maskingBar = document.querySelector("#tl-scroll-prog-mask");
+
+    markers.forEach((marker, i) => {
+        marker.addEventListener("click", e => {
+            gsap.to(maskingBar, {
+                scaleY: markersPositionPercentage[i],
+            })
+        });
+    });
 }
