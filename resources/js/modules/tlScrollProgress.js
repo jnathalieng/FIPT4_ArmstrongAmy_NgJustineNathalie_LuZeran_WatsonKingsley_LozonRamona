0e8d7f51c2a5bb7e631c6a5361cf3d16f9609b89
Zeran Lu <zeranlu0326@gmail.com>
2026-03-26 16:39:36 -0400

    JS: added mask scroll progress JS logic
    also adjusted minor CSS positioning settings

--- resources/js/modules/tlScrollProgress.js
+++ resources/js/modules/tlScrollProgress.js
@@ -1,19 +1,18 @@
 export function tlScrollProgress() {
     gsap.registerPlugin(ScrollTrigger);
     
-    gsap.set("#tl-scroll-prog-fill", {
-        opacity: 0.2,
-    })
+    gsap.set("#tl-scroll-prog-mask", {
+        transform: "scaleY(0)"
+    });
 
-    gsap.to("#tl-scroll-prog-fill", {
-        opacity: 1,
+    gsap.to("#tl-scroll-prog-mask", {
+        scaleY: 1,
         ease: "none",
         scrollTrigger: {
             trigger: "#timeline-section",
             start: "top top",
             end: "bottom bottom",
-            scrub: true,
-            markers: true
+            scrub: 2
         }
     })
 } 
