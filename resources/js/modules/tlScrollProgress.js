3b6078711b03f092d715f5e7bbb64ad4718e5d1c
Zeran Lu <zeranlu0326@gmail.com>
2026-03-26 16:04:56 -0400

    JS: defined scroll progress indicator scroll trigger JS

--- resources/js/modules/tlScrollProgress.js
+++ resources/js/modules/tlScrollProgress.js
@@ -1,5 +1,15 @@
 export function tlScrollProgress() {
     gsap.registerPlugin(ScrollTrigger);
-
     
+    gsap.to("#tl-scroll-prog-fill", {
+        opacity: 1,
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#timeline-section",
+            start: "top top",
+            end: "bottom bottom",
+            scrub: true,
+            markers: true
+        }
+    })
 } 
