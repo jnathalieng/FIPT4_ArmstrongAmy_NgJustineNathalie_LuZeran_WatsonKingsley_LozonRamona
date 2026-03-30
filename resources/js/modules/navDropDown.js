59627760f7133e5ac4712f959dd1ff1d2d00481a
Zeran Lu <zeranlu0326@gmail.com>
2026-03-30 17:34:31 -0400

    JS: adjusted classList adding and removing logic to include nav bar as well to keep fluid navbar control

--- resources/js/modules/navDropDown.js
+++ resources/js/modules/navDropDown.js
@@ -1,12 +1,13 @@
 export function navDropDown() {
     const dropDownWrap = document.querySelector(".drop-down-wrap");
     const dropDownMenu = document.querySelector(".dropdown-menu");
+    const desktopNav = document.querySelector(".desktop-nav");
+
+    desktopNav.addEventListener("mouseleave", () => {
+        dropDownMenu.classList.remove("droppingDown");
+    })
 
     dropDownWrap.addEventListener("mouseenter", () => {
         dropDownMenu.classList.add('droppingDown');
-    });
-
-    dropDownWrap.addEventListener("mouseleave", () => {
-        dropDownMenu.classList.remove("droppingDown");
     });
 }
