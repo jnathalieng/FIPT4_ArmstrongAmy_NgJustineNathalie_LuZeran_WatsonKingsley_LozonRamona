f9201ee2aa59e2863ec34cf249dc8f051945b70b
Zeran Lu <zeranlu0326@gmail.com>
2026-03-30 17:11:54 -0400

    JS: added classList adding and removing logic for dropdown menu and dropdown warpper

--- resources/js/modules/navDropDown.js
+++ resources/js/modules/navDropDown.js
@@ -1,3 +1,12 @@
 export function navDropDown() {
+    const dropDownWrap = document.querySelector(".drop-down-wrap");
+    const dropDownMenu = document.querySelector(".dropdown-menu");
 
+    dropDownWrap.addEventListener("mouseenter", () => {
+        dropDownMenu.classList.add('droppingDown');
+    });
+
+    dropDownWrap.addEventListener("mouseleave", () => {
+        dropDownMenu.classList.remove("droppingDown");
+    });
 }
