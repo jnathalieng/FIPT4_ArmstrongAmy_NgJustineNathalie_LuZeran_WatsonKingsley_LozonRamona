f90c513e2cf3fec272ebd75ff0103babd6380b6d
Zeran Lu <zeranlu0326@gmail.com>
2026-04-06 22:04:18 -0400

    JS: created hotspot scrollintoView logic

--- resources/js/modules/mapHotSpots.js
+++ resources/js/modules/mapHotSpots.js
@@ -1,3 +1,21 @@
 export function mapHotSpots() {
-    
+
+
+    const hotspots = document.querySelectorAll("#hotspots circle");
+
+    hotspots.forEach((hotspot, index) => {
+        const hsID = index + 1;
+        const hsTarget = document.querySelector(`[data-target="base-${hsID}"]`);
+
+        if (!hsTarget) return;
+
+        hotspot.style.cursor = 'pointer';
+
+        hotspot.addEventListener('click', () => {
+
+            hsTarget.scrollIntoView({
+                behavior: 'smooth'
+            });
+        })
+    })
 }
