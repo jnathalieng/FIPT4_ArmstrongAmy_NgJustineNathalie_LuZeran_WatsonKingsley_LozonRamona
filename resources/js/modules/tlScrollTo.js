b041a0e56425636af794204152d9ce8f38cb85e5
Zeran Lu <zeranlu0326@gmail.com>
2026-04-07 11:03:26 -0400

    JS: added scrollTO

--- resources/js/modules/tlScrollTo.js
+++ resources/js/modules/tlScrollTo.js
@@ -1,3 +1,22 @@
 export function tlScrollTo() {
-    
+    gsap.registerPlugin(ScrollToPlugin);
+
+    const markers = document.querySelectorAll(".tl-marker");
+
+    markers.forEach(marker => {
+        marker.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            const target = marker.getAttribute("href");
+
+            gsap.to(window, {
+                duration: 1,
+                scrollTo: {
+                    y: target,
+                    offsetY: 100
+                },
+                ease: "power2.inOut"
+            });
+        });
+    });
 }
