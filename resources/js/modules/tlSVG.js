b6707aae85b804b5630191699346e4d9c35824fa
Zeran Lu <zeranlu0326@gmail.com>
2026-04-07 10:00:28 -0400

    JS: added all tl-content-breaks as an arrayed animation state

--- resources/js/modules/tlSVG.js
+++ resources/js/modules/tlSVG.js
@@ -1,3 +1,15 @@
 export function tlSVG() {
-    
+    gsap.utils.toArray(".tl-content-break").forEach((section) => {
+        
+        const svgs = section.querySelectorAll("svg");
+
+        tlSVG.fromTo(svgs, {
+            clipPath: "inset(0 100% 0 0)",            
+        },
+        {
+            clipPath: "inset(0 0 0 0)",
+            duration: 1,
+            ease: "power2.inOut",
+        });
+    });
 }
