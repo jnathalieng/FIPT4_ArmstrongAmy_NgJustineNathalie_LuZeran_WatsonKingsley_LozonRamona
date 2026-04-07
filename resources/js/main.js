1094a2c7f5e48f68e0749143504f9d98d66ba6ab
Zeran Lu <zeranlu0326@gmail.com>
2026-04-07 01:49:05 -0400

    JS: commented out previous mapContentSlide() js

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,7 +8,7 @@ import gallery from "./modules/gallery.js";
 import blog from "./modules/blog.js";
 import { createApp } from 'vue';
 import { mapScroll } from "./modules/mapScroll.js";
-import { mapContentSlide } from "./modules/mapContentSlide.js";
+// import { mapContentSlide } from "./modules/mapContentSlide.js";
 import { mapHotSpots } from "./modules/mapHotSpots.js";
 import { footerSearch } from "./modules/footer.js";
 import { dossierOpen } from "./modules/dossierOpen.js";
@@ -48,7 +48,7 @@ else if(document.body.dataset.page === "trainingBases") {
     videoQuery();
     
     mapScroll();
-    mapContentSlide();
+    // mapContentSlide();
     mapHotSpots();
 }
 else if(document.body.dataset.page === "BOB") {
