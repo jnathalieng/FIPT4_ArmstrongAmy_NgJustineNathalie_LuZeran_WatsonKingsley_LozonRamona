bdd70d2ec802e262a0742d76143641491f5cedc6
Zeran Lu <zeranlu0326@gmail.com>
2026-04-13 14:07:14 -0400

    created events_table columns/variables

--- database/migrations/2026_04_13_165120_create_events_table.php
+++ database/migrations/2026_04_13_165120_create_events_table.php
@@ -13,6 +13,22 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('events_title');
+            $table->text('events_description');
+
+            $table->dateTime('events_start_datetime');
+            $table->dateTime('events_end_datetime')->nullable();
+            $table->string('events_timezone')->nullable();
+
+            $table->string('events_category');
+            $table->string('events_status');
+
+            $table->string('events_image_1')->nullable();
+            $table->string('events_image_alt_1')->nullable();
+            $table->string('events_image_2')->nullable();
+            $table->string('events_image_alt_2')->nullable();
+            $table->string('events_image_3')->nullable();
+            $table->string('events_image_alt_3')->nullable();
             $table->timestamps();
         });
     }
