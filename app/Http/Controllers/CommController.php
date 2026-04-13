a84d46694092d9a37141c31e0647f2c24dc4a28e
Zeran Lu <zeranlu0326@gmail.com>
2026-04-13 17:28:43 -0400

    added Commemorative card CRUD

--- app/Http/Controllers/CommController.php
+++ app/Http/Controllers/CommController.php
@@ -19,4 +19,30 @@ class CommController extends Controller
             'trainingFour' => $trainingFour,
             ]);
     }
+
+    // COMMEMORATING CARDS CRUD FUNCTIONALITY BLOCK
+    public function storeComm(Request $request)
+    {
+        // create a new comm card
+        $comm = Comm::create($request->all());
+
+        return response()->json($comm);
+    }
+
+    public function updateComm(Request $request, $id)
+    {
+        // updating a comm3 card
+        $comm = Comm::findOrFail($id);
+        $comm->update($request->all());
+        
+        return response()->json($comm);
+    }
+
+    public function destroyComm($id)
+    {
+        Comm::findOrFail($id)->delete();
+        
+        return response()->json(['message' => 'This commemorative card has been deleted successfully.']);
+    }
+
 }
