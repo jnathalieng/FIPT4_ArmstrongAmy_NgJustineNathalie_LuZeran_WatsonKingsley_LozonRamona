a66ebe66ab2e898c2a5ad3d372ba5652d660f9ee
Zeran Lu <zeranlu0326@gmail.com>
2026-04-13 14:24:53 -0400

    added index() function

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -8,4 +8,12 @@ use App\Models\Events;
 class EventsController extends Controller
 {
     //
+    public function index() 
+    {
+        // orders events - upcoming events first
+        $events = Events::orderBy('events_start_datetime', 'asc')->get();
+
+        return response()->json($events);
+    }
+    
 }
