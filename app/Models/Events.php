78470ec288822ffd9b400f57f876ddcf3c78b71f
Zeran Lu <zeranlu0326@gmail.com>
2026-04-13 14:15:41 -0400

    updated events model file with appropriate migration variables

--- app/Models/Events.php
+++ app/Models/Events.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Events extends Model
 {
     //
+    protected $fillable = [
+        'events_title',
+        'events_description',
+        'events_start_datetime',
+        'events_end_datetime',
+        'events_timezone',
+        'events_category',
+        'events_status',
+        'events_image_1',
+        'events_image_alt_1',
+        'events_image_2',
+        'events_image_alt_2',
+        'events_image_3',
+        'events_image_alt_3'
+    ];
 }
