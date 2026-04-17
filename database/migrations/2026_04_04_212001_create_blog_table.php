4b964af45f4b89dc57bf323c8f92016226dbda92
Ramona <144703303+Ramona-Lozon@users.noreply.github.com>
2026-04-17 15:30:36 -0400

    debugged images in events and blog

--- database/migrations/2026_04_04_212001_create_blog_table.php
+++ database/migrations/2026_04_04_212001_create_blog_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('slug')->unique();
             $table->text('content');
             $table->text('excerpt');
             $table->string('featured_image')->nullable();
