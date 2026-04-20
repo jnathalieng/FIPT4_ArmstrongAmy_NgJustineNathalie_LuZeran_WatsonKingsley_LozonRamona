d7b7abf359d8909021d8707fb66239ac53df8ab8
Zeran Lu <zeranlu0326@gmail.com>
2026-04-20 00:46:29 -0400

    removed and reformated html elemetns

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -191,30 +191,25 @@
         </div>
 
     <!-- About Video Section -->
-    <section class="grid-con" id="about-video-section">
-        
-        <h2>Preserving London Aviation's History</h2>
-
-        <p>
-            <br>
-            Inside the 427 Wing Museum, exploring the people, stories, and vision behind London's aviation legacy.
-            <br>
-        </p>
-
-        <div class="col-span-full m-col-start-2 m-col-span-10" id="about-video-con">
-            <video controls playsinline preload="metadata" disablepictureinpicture disableremoteplayback controlslist="nodownload nofullscreen noremoteplayback" id="about-video">
-                <source src="{{ asset('videos/website-video.mp4') }}" type="video/mp4">
-            </video>
-        </div>
-
-        <p class="caption col-span-full">A Conversation with Museum Curator Sir Mike Baker</p>
-        
-    </section>
-
-    <!-- About Mike Section -->
-    <section class="text-con">
+    <section class="text-con" id="about-video-section">
         
         <div class="text-box">
+            <h2>Preserving London Aviation's History</h2>
+
+            <p>
+                <br>
+                Inside the 427 Wing Museum, exploring the people, stories, and vision behind London's aviation legacy.
+                <br>
+            </p>
+
+            <div class="about-video-con">
+                <video controls playsinline preload="metadata" disablepictureinpicture disableremoteplayback controlslist="nodownload nofullscreen noremoteplayback" id="about-video">
+                    <source src="{{ asset('videos/website-video.mp4') }}" type="video/mp4">
+                </video>
+            </div>
+
+            <p class="caption">A Conversation with Museum Curator Sir Mike Baker</p>
+
             <p class="quote">“The losses in training are really just as much as a sacrifice in winning the war as losses on the battlefield.” - Mike</p>
 
             <picture>
