4003bebc57ae3d287eeb8dd03ce82bd7c56cf65d
Zeran Lu <zeranlu0326@gmail.com>
2026-04-23 02:48:34 -0400

    added skew functionality

--- resources/js/modules/cardSkew.js
+++ resources/js/modules/cardSkew.js
@@ -1,5 +1,15 @@
 export function cardSkew() {
     console.log("check out the cards");
 
-    
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.to(".comm-card-box", {
+        skewY: 5,
+        scrollTrigger: {
+            trigger: ".comm-card-box",
+            start: "top bottom",
+            end: "bottom top",
+            scrub: true 
+        }
+    });
 }
