b37ca39f3964725b984f0393b771719fd94871ef
Zeran Lu <zeranlu0326@gmail.com>
2026-04-23 02:54:55 -0400

    adjusted skew to be able pick up and render based on a clamped velocity

--- resources/js/modules/cardSkew.js
+++ resources/js/modules/cardSkew.js
@@ -3,13 +3,40 @@ export function cardSkew() {
 
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.to(".comm-card-box", {
-        skewY: 5,
-        scrollTrigger: {
-            trigger: ".comm-card-box",
-            start: "top bottom",
-            end: "bottom top",
-            scrub: true 
+    let proxy = {skew: 0};
+
+    let skewSetter = gsap.quickSetter(".comm-card-box", "skewY", "deg");
+
+    let clamp = gsap.utils.clamp(-10, 10);
+
+    ScrollTrigger.create({
+        onUpdate: (self) => {
+            let skew = clamp(self.getVelocity() / -300);
+
+            if (Math.abs(skew) > Math.abs(proxy.skew)) {
+                proxy.skew = skew;
+                gsap.to(proxy, {
+                    skew: 0,
+                    duration: 0.3,
+                    ease: "power3",
+                    overwrite: true,
+                    onUpdate: () => skewSetter(proxy.skew)
+                });
+            }
         }
     });
+
+    gsap.set(".comm-card-box", {
+        transformOrigin: "right center",
+        force3D: true
+    });
+    // gsap.to(".comm-card-box", {
+    //     skewY: 5,
+    //     scrollTrigger: {
+    //         trigger: ".comm-card-box",
+    //         start: "top bottom",
+    //         end: "bottom top",
+    //         scrub: true 
+    //     }
+    // });
 }
