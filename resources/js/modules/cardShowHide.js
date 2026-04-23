1eadc6c9bd4e45426ddacfa5322a928edeb1c35a
Zeran Lu <zeranlu0326@gmail.com>
2026-04-22 21:44:56 -0400

    added a !== check to see and remove other cards with the "active" class

--- resources/js/modules/cardShowHide.js
+++ resources/js/modules/cardShowHide.js
@@ -2,7 +2,20 @@ export function cardShowHide() {
     console.log("click on a card!");
     
     document.querySelectorAll(".comm-card-box").forEach(card => {
+
         card.addEventListener('click', function() {
+
+        document.querySelectorAll(".comm-card-box").forEach(cards => {
+            if (cards !== this) {
+                cards.classList.remove('active');
+
+                const textContents = cards.querySelector('.comm-p, .comm-text-section');
+
+                if (textContents) {
+                    textContents.classList.remove('active');
+                }
+            }
+        });
             
             this.classList.toggle('active');
 
@@ -12,5 +25,6 @@ export function cardShowHide() {
                 textContent.classList.toggle('active');
             }
         });
+
     });
 }
