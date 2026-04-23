143c8d01322c99c9130659ca197f014eb8f6ec3a
Zeran Lu <zeranlu0326@gmail.com>
2026-04-23 11:39:28 -0400

    canteent header

--- resources/views/canteen.blade.php
+++ resources/views/canteen.blade.php
@@ -16,7 +16,7 @@
 <body data-page="canteen" id="canteen-page">
 
     <header class="header">
-    <!-- navbar -->
+        <!-- navbar -->
         <nav class="desktop-nav">
             <ul>
                 <li class="desktop-logo-con m-l-nav-item">
@@ -61,7 +61,7 @@
 
                 <li class="m-l-nav-item drop-down-wrap" id="events-nav">
                     
-                    <a class="nav-anchor" href="{{ route('events') }}">Events</a>
+                    <a class="nav-anchor" href="{{ route('gallery') }}">Events</a>
 
                     <ul class="dropdown-menu">
                         <li class="dropdown-item">
@@ -72,6 +72,7 @@
                         </li>
                     </ul>
                 </li>
+
                 <li class="m-l-nav-item">
                     <a class="nav-anchor" href="{{ route('contact') }}">Contact</a>
                 </li>
@@ -79,10 +80,15 @@
         </nav>
 
         <!-- mobile hamburger menu -->
-        <div class="hamburger">
-            <span></span>
-            <span></span>
-            <span></span>
+        <div class="hamburger-bar">
+            <a href="{{ route('home') }}" class="ham-logo-con">
+                <img src="{{ asset('images/icons/logos/SVG_FILES_WHITE/FINAL_LOGOWHITE.svg') }}" alt="image of logo">
+            </a>
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </div>
 
         <nav class="hamburger-nav">
@@ -118,6 +124,12 @@
                 </li>
                 <li class="mobile-nav-li">
                     <a class="mobile-nav-item" href="{{ route('events') }}">Events</a>
+                </li>
+                <li class="mobile-nav-li">
+                    <a class="mobile-nav-item" href="{{ route('blog') }}">Blogs</a>
+                </li>
+                <li class="mobile-nav-li">
+                    <a class="mobile-nav-item" href="{{ route('gallery') }}">Gallery</a>
                 </li>
                 <li class="mobile-nav-li">
                     <a class="mobile-nav-item" href="{{ route('contact') }}">Contact</a>
