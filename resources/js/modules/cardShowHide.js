9d76ca890d8f661c3d23d643db61cba6be1984b0
Zeran Lu <zeranlu0326@gmail.com>
2026-04-22 21:18:19 -0400

    added JS show hide functionality on cards

--- resources/js/modules/cardShowHide.js
+++ resources/js/modules/cardShowHide.js
@@ -1,3 +1,16 @@
 export function cardShowHide() {
     console.log("click on a card!");
+    
+    document.querySelectorAll(".comm-card-box").forEach(card => {
+        card.addEventListener('click', function() {
+            
+            this.classList.toggle('active');
+
+            const textContent = this.querySelector('.comm-p, .comm-text-section');
+
+            if (textContent) {
+                textContent.classList.toggle('active');
+            }
+        });
+    });
 }
